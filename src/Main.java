3b51350e9d70925546611b9a74c661649b96f143
Пользователь <dinarkatttb16@gmail.com>
2024-04-28 22:06:52 +0500

    string hw done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+
+        System.out.println("Task 2");
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчёта - " + fullName.toUpperCase());
+
+        System.out.println("Task 3");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName.replace("ё", "е"));
+
+
 
     }
 }
